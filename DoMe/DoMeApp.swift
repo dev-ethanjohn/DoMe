7cba463fd9cc3def9c0da59f4baab12b5c3a1cb6
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-02-02 21:45:57 +0800

    Initial Commit

---
+++ DoMe/DoMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoMeApp.swift
+//  DoMe
+//
+//  Created by Ethan John Paguntalan on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DoMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
