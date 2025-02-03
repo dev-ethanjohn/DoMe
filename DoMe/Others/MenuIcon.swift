cc9735c7ec1f93a82df9da614239a0c0fc2bbff0
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-02-03 20:23:22 +0800

    Implement fluid side menu animation with home parallax shift

---
+++ DoMe/Others/MenuIcon.swift
@@ -0,0 +1,18 @@
+//
+//  MenuIcon.swift
+//  DoMe
+//
+//  Created by Ethan John Paguntalan on 2/3/25.
+//
+
+import SwiftUI
+
+struct MenuIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuIcon()
+}
